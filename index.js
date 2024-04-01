2b88e9349e9ecc880131679bd5951d42add75ce2
BUBBYSAKA <143959496+BUBBYSAKA@users.noreply.github.com>
2024-04-01 22:42:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,5 +12,127 @@ bot.on("ready", () => {
     name: "discord.gg/permfruits"
   }).catch(console.error);
 });
+bot.on('messageCreate', message => {
+	const channel = client.channels.cache.get('1144933431910871052'); // Replace with the desired channel ID
+	console.log(message.content);
+
+  if (message.content.startsWith("ZADE")) {
+    return message.reply("Suck My dick ZADE");
+  }
+
+  if (message.content === "RYANN") {
+    message.reply("@rayyan#9435 :shocked:");
+  }
+if (message.content === "doriaSucks") {
+	var i = 0 ;
+	while(i < 500){
+    message.channel.send("doria is a bad guy ");
+    i++;
+    }
+  }
+  if (message.content === "HOLYCIT") {
+    message.channel.send("can you not");
+  }
+  if(message.content.includes("chatgpt")) {
+    message.channel.send("FATGPT is better than ChatGPT");
+    }
+    if(message.content.includes("azzypic")) {
+      return message.channel.send("https://media.discordapp.net/attachments/1095663813296136232/1097752107609763922/Untitled76_20230418131434.png?width=447&height=447")
+    }
+   /* if (channel && !message.author.bot) {
+      channel.send(message.author.tag + ": "  + message.content);
+    } */
+    if (message.content.startsWith('!dmc')) {
+    // Find the user to send the DM to
+    const user = message.mentions.users.first();
+
+    if (user) {
+      // Remove the "!dm @user" part from the message content
+      const messageContent = message.content.slice(26);
+
+      // Send a DM to the user
+      user.send(messageContent)
+        .then(() => {
+          message.reply(`Sent a DM to ${user.tag}`);
+        })
+        .catch(error => {
+          message.reply(`Couldn't send a DM to ${user.tag}`);
+          console.error(error);
+        });
+    } else {
+      message.reply('You need to mention a user to send a DM.');
+    }
+  }
+
+
+  if (message.content.startsWith('!dms')) {
+    const content = message.content.slice(4); // Remove the "!dm " part
+    const userId = content.match(/\d+/); // Extract user ID from the content
+
+    if (userId) {
+      const user = client.users.cache.get(userId[0]); // Get the user object
+      const dmContent = content.replace(`<@${userId}>`, ''); // Remove the user mention
+      if (user) {
+        user.send(dmContent)
+          .then(() => {
+            message.reply(`Sent a DM to ${user.tag}`);
+          })
+          .catch(error => {
+            message.reply(`Couldn't send a DM to ${user.tag}`);
+            console.error(error);
+          });
+      } else {
+        message.reply('User not found.');
+      }
+    } else {
+      message.reply('Invalid format. Use: !dm <@USER_ID> Your message');
+    }
+  }
+if (message.content.startsWith(':') && message.content.endsWith(':')) {
+    // Replace 'OTHER_SERVER_ID' with the ID of the server where the emoji is located
+    const otherServerId = '1069108304929886208';
+
+    // Replace 'EMOJI_NAME' with the name of the emoji you want to use
+    const emojiName = message.content.slice(1, -1);
+
+    // Find the Guild (server) object of the other server by its ID
+    const otherServer = client.guilds.cache.get(otherServerId);
+
+    if (otherServer) {
+      // Find the emoji by name in the other server's emoji cache
+      const emoji = otherServer.emojis.cache.find(e => e.name === emojiName);
+      const sticker = otherServer.stickers.cache.find(
+        s => s.name === emojiName || s.id === emojiName
+      );
+
+      if (emoji) {
+        // Send the emoji as a message
+        message.channel.send(emoji.toString());
+      } else if (sticker) {
+        // Send the sticker as a message
+        message.channel.send(`Sticker: ${sticker.toString()}`);
+      } else {
+        message.reply('Emoji or sticker not found in the other server.');
+      }
+    } else {
+      message.reply('Server not found.');
+    }
+  }
+});
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.on('line', userInput => {
+  // Send the user's input as a message to the specified channel
+  const channel = client.channels.cache.get("1223919638044344430");
+
+  if (channel) {
+    channel.send(userInput);
+  } else {
+    console.error('Channel not found.');
+  }
+  });
 
 bot.connect(); // Get the bot to connect to Discord
