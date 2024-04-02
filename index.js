b36ca2da2073ef6cd2e7f450ff0213196cda45e4
BUBBYSAKA <143959496+BUBBYSAKA@users.noreply.github.com>
2024-04-02 22:10:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,10 @@ bot.once("ready", () => {
   console.log("Ready");
 });
 bot.on("ready",() => {
-	while(doxmsg){
 	myChannel = bot.channels.cache.get("1223919638044344430");
+	myChannel.send("Happy Happy Happy");
+	while(doxmsg){
+	
 	myChannel.send(doxmsg);
 	doxmsg = null ;
 	}
