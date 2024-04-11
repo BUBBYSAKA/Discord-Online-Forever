1d1f488678b310d8cc63f979f051aafec0245377
BUBBYSAKA <143959496+BUBBYSAKA@users.noreply.github.com>
2024-04-11 23:46:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -218,6 +218,6 @@ function sendDoxMessage(channel) {
 
 
 MySelfBot.login(process.env.token2);
-bot.login(process.env.tokn);
+bot.login(process.env.token);
 
 account.connect(); // Get the bot to connect to Discord
